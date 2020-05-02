00488b493f487a27c22d2d24754545bb894f06ab
Fede Herrera <fedega.herrera@gmail.com>
2020-05-02 18:05:40 -0300

    Agregué varias funciones que pueden ser de utilidad del fs
    Ahora se puede inicializar el fs. Se crean directorios principales.
    Falta llenar el bitmap y ver blocks

--- GameCard/GameCard.c
+++ GameCard/GameCard.c
@@ -8,6 +8,18 @@ int main(void){
 	//Servidor iniciado
 	printf("Server ready for action! \n");
 
+
+
+
+
+
+	iniciar_punto_de_montaje(path_de_tallgrass);
+
+
+
+
+
+
 	while(1){
 	int cliente = esperar_cliente(socket_servidor,logger);
 
@@ -86,20 +98,20 @@ void procesar_solicitud(int cod_op, int cliente_fd) {
 	//void* msg;
 
 		switch (cod_op) {
-		case GET:
+		case t_GET:
 			log_info(logger,"Me llegaron mensajes de Suscriber get");
 			//Hacer algo con get
 			//msg = recibir_mensaje(cliente_fd, &size);
 			//free(msg);
 			break;
 
-		case CATCH:
+		case t_CATCH:
 			log_info(logger,"Me llegaron mensajes de Suscriber Catch");
 			//Hacer lo que debia hacerse cuando se recibiera un catch
 			//msg = recibir_mensaje(cliente_fd, &size);
 			//free(msg);
 			break;
-		case NEW:
+		case t_NEW:
 			log_info(logger,"Me llegaron mensajes de Suscriber New");
 			//Hacer lo que debiera hacerse con new
 			//msg = recibir_mensaje(cliente_fd, &size);
@@ -132,4 +144,325 @@ void setearValoresDeGame(t_config *config){
 	path_de_tallgrass = config_get_string_value(config,"PUNTO_MONTAJE_TALLGRASS");
 	tiempo_de_reconexion = config_get_int_value(config,"TIEMPO_DE_REINTENTO_CONEXION");
 	tiempo_de_reintento_operacion = config_get_int_value(config,"TIEMPO_DE_REINTENTO_OPERACION");
+}
+
+
+//////////////////////////////////////////////////
+//////////////	UTILIDADES PARA DAR INICIO AL FS
+////////////////////////////////////////////////////
+
+
+// da inicio al punto de montaje
+//
+
+void iniciar_punto_de_montaje(char *puntoDeMontaje){
+		carpetas_iniciar(puntoDeMontaje);
+
+		//Creamos un string vacio
+		char* auxConfigMetadata = string_new();
+
+		//Cargamos el string con la ruta del metadata.bin del FS
+		string_append(&auxConfigMetadata,RUTA_DE_METADATA_MONTAJE);
+		string_append(&auxConfigMetadata,"/Metadata.bin");
+
+		config_fs = config_create(auxConfigMetadata);
+
+		// Vemos si existe el .bin con la configuracion del FS y cargamos el config a las variables globales
+		// En el caso que no exista la carpeta de configuracion crea una por default (Con el ejemplo que estaba en el pdf)
+		if (config_fs == NULL || config_keys_amount(config_fs) == 0){
+			inicio_default(puntoDeMontaje);
+		}
+
+
+		carga_config_fs();
+		bloques_iniciar();
+		mostrarRutas();
+
+		// Limpieza de variables
+
+		free(auxConfigMetadata);
+}
+
+// Da un inicio default con la configuracion que esta en el pdf
+void inicio_default(char* puntoDeMontaje){
+
+	FILE* archivoAux;
+
+	// Cargamos la ruta de metadata.bin
+	// Y creamos el archivo metadata.bin VACIO
+	char* auxMetadata = string_new();
+	string_append(&auxMetadata,puntoDeMontaje);
+	string_append(&auxMetadata,"/Metadata/Metadata.bin");
+	archivoAux = fopen(auxMetadata,"a");
+	fclose(archivoAux);
+
+	// Tratamos al archivo metadata.bin como un config
+	// Y le agregamos la configuracion del pdf como default
+	// Tamaño del bloque 64
+	// cantidad de bloques 5192
+	// Numero mágico TALL_GRASS
+	config_fs = config_create(auxMetadata);
+	config_set_value(config_fs, "BLOCK_SIZE", "10");
+	config_set_value(config_fs, "BLOCKS", "5");
+	config_set_value(config_fs, "MAGIC_NUMBER", "TALL_GRASS");
+	config_save(config_fs);
+
+
+	// Se crea el archivo del bitmap
+	char* auxBitMap = string_new();
+	string_append(&auxBitMap,puntoDeMontaje);
+	string_append(&auxBitMap,"/Metadata/Bitmap.bin");
+	archivoAux = fopen(auxBitMap,"a");
+	fclose(archivoAux);
+	free(auxMetadata);
+	free(auxBitMap);
+}
+
+// Inicializa los bloques
+// Por ahora solo los inicia en 0 y sobreescribe. Hay que ver como hacer para que no
+// sobreescriba datos anteriores
+void bloques_iniciar(){
+	FILE *bloque;
+	int i;
+	// Se crean los bloques pero hay un problema porque elimina los bloques
+	// que anteriormente fueron creados
+	// en esta funcion tengo mucha perdida de memoria. Revisar
+	for(i = 1; i <= BLOQUES_DE_BITMAP ; i++){
+		printf("Creo el bloque %i \n",i);
+		char* bloque_a_crear = string_new();
+		string_append(&bloque_a_crear,RUTA_DE_BLOQUES);
+		string_append(&bloque_a_crear,"/");
+		string_append(&bloque_a_crear,string_itoa(i));
+		string_append(&bloque_a_crear,".bin");
+		bloque = fopen(bloque_a_crear,"a");
+		fclose(bloque);
+		free(bloque_a_crear);
+	}
+}
+
+
+//	Crea las carpetas que debe tener el fs (las carpetas bases)
+// 	que son metadata,files y blocks.
+void carpetas_iniciar(char* puntoDeMontaje){
+
+	// Se crea la carpeta de metadata
+	RUTA_DE_METADATA_MONTAJE = string_new();
+	string_append(&RUTA_DE_METADATA_MONTAJE,puntoDeMontaje);
+	string_append(&RUTA_DE_METADATA_MONTAJE,"/Metadata");
+	mkdir(RUTA_DE_METADATA_MONTAJE,0777);
+	// Se crea la carpeta de FILES
+	RUTA_DE_FILES = string_new();
+	string_append(&RUTA_DE_FILES,puntoDeMontaje);
+	string_append(&RUTA_DE_FILES,"/Files");
+	mkdir(RUTA_DE_FILES,0777);
+
+	// Se crea la carpeta pokemon dentro de files
+	RUTA_DE_POKEMON = string_new();
+	string_append(&RUTA_DE_POKEMON,RUTA_DE_FILES);
+	string_append(&RUTA_DE_POKEMON,"/Pokemon");
+	mkdir(RUTA_DE_POKEMON,0777);
+
+	// Se crea el metadata del pokemon diciendo que es Directorio
+	FILE* metaPKM = fopen(obtener_direccion_metadata(RUTA_DE_POKEMON),"a");
+	fclose(metaPKM);
+	t_config* metadataDeCarpetaPokemon = obtener_metadata_de_ruta(RUTA_DE_POKEMON);
+	config_set_value(metadataDeCarpetaPokemon,"DIRECTORY","Y");
+	config_save(metadataDeCarpetaPokemon);
+	config_destroy(metadataDeCarpetaPokemon);
+
+
+	// Se crea la carpeta de Blocks
+	RUTA_DE_BLOQUES = string_new();
+	string_append(&RUTA_DE_BLOQUES,puntoDeMontaje);
+	string_append(&RUTA_DE_BLOQUES,"/Blocks");
+	mkdir(RUTA_DE_BLOQUES,0777);
+}
+
+
+// Carga la configuracion que está en el metadata.bin
+// del fs. Toma el tamaño de bloque, bloques del bitmap y el magicnumber
+void carga_config_fs(){
+	TAMANIO_DE_BLOQUE = config_get_int_value(config_fs,"BLOCK_SIZE");
+	BLOQUES_DE_BITMAP = config_get_int_value(config_fs,"BLOCKS");
+	MAGIC_NUMBER = config_get_string_value(config_fs,"MAGIC_NUMBER");
+}
+
+
+////////////////////////////////////////////////
+///////////////////////UTILIDADES VARIAS
+////////////////////////////////////////////////
+
+
+
+// MUESTRA LAS RUTAS AUNQUE ESTARIA BUENO USAR EL LOG(UNO CAPAZ PARA NOSOTROS) PARA INFORMARs
+// NO SIRVE PARA EL TP PERO PODEMOS VERIFICAR COSAS CON ESTO
+void mostrarRutas(){
+	printf("------------------------\n");
+		printf("Mostrando rutas de trabajo \n");
+		printf("Metadata: %s \n",RUTA_DE_METADATA_MONTAJE);
+		printf("Files: %s \n",RUTA_DE_FILES);
+		printf("Blocks: %s \n",RUTA_DE_BLOQUES);
+		printf("\n");
+		printf("Mostrando config del FS \n");
+		printf("El tamaño del bloque del metadata es: %i \n",TAMANIO_DE_BLOQUE);
+		printf("La cantidad de bloques es: %i \n",BLOQUES_DE_BITMAP);
+		printf("El magic number es: %s \n",MAGIC_NUMBER);
+}
+
+// Retorna el nombre del archivo dado el path
+// Es decir retorna el último string despues del último "/"
+// PARA FUSE
+char* obtener_nombre_de_archivo(char *path){
+	char **aux;
+	char *nombre;
+
+	aux = string_split(path, "/");
+
+	// Le restamos dos ya si hacemos -1 retornamos NULL por el string split
+	// y el -2 retorna el último nombre
+
+	nombre = aux[(string_length(*aux) - 2)];
+	free(aux);
+	return nombre;
+}
+
+//Agrega la direccion del metadata de la ruta ingresada
+char* obtener_direccion_metadata(char *path){
+
+	char* rutaMetadata;
+	rutaMetadata = string_new();
+
+	string_append(&rutaMetadata,path);
+	string_append(&rutaMetadata,"/Metadata.bin");
+
+	return rutaMetadata;
+}
+
+//	retorna el metadata del archivo
+// y hay que tratarlo como un config
+// Si retorna NULL no existe la metadata o la ruta
+t_config* obtener_metadata_de_ruta(char *path){
+	char* rutaMetadata;
+	t_config *metadata;
+	rutaMetadata = obtener_direccion_metadata(path);
+
+	metadata = config_create(rutaMetadata);
+	free(rutaMetadata);
+	return metadata;
+}
+
+// Mediante una ruta retorna si es un directorio o registro
+// 1 si es registro
+// 2 si es directorio
+// -1 si es error
+
+int esDirectorio(char* ruta){
+	t_config* unMeta;
+	char* tipoDeArchivo;
+	unMeta = obtener_metadata_de_ruta(ruta);
+	if(unMeta == NULL) return -1;
+
+	tipoDeArchivo = config_get_string_value(unMeta,"DIRECTORY");
+
+	if(string_equals_ignore_case(tipoDeArchivo,"Y")){
+		config_destroy(unMeta);
+		return 2;
+	}
+	if(string_equals_ignore_case(tipoDeArchivo,"N")){
+		config_destroy(unMeta);
+		return 1;
+	}
+	else return -1;
+}
+
+
+////////////////////////////////////////////////
+////////////////////////// COSAS DEL POKEMON
+//////////////////////////////////////////
+
+// Retorna el metada de un pokemon
+// dado solo su nombre (Puede retornar el metadata del directorio, cuidado)
+//
+t_config* obtener_metadata_de_pokemon(char *nombrePokemon){
+	char *ruta_pokemon;
+	t_config* configPokemon;
+
+	ruta_pokemon = string_new();
+	string_append(&ruta_pokemon,RUTA_DE_FILES);
+	string_append(&ruta_pokemon,"/Pokemon/");
+	string_append(&ruta_pokemon,nombrePokemon);
+
+	configPokemon = config_create(ruta_pokemon);
+	free(ruta_pokemon);
+
+	return configPokemon;
+}
+
+// Busca si existe el pokemon buscado encontrando su metadata
+// Aunque se encuentra en fase de prueba ya que si existe solo el directorio
+// va a retornar que si existe el pokemon
+// Retorna 0 si no existe el pokemon
+// sino retorna 1 si existe
+
+bool existePokemon(char* nombrePokemon){
+	t_config *metadataPokemon;
+
+	metadataPokemon = obtener_metadata_de_pokemon(nombrePokemon);
+
+	if(metadataPokemon == NULL) {
+		return 0;
+	}
+
+	config_destroy(metadataPokemon);
+	return 1;
+}
+
+//	Crea un archivo con su metadata del nombre del pokemon
+//	Obviamente el archivo va a estar vacio hasta ser llenado
+void crearPokemon(char* nombrePokemon){
+	char* path_pokemon = string_new();
+	string_append(&path_pokemon,RUTA_DE_POKEMON);
+	string_append(&RUTA_DE_POKEMON,"/");
+	string_append(&RUTA_DE_POKEMON,nombrePokemon);
+	mkdir(path_pokemon,0777);
+
+	string_append(&RUTA_DE_POKEMON,"/Metadata.bin");
+	FILE *archivoVacioMetadata = fopen(RUTA_DE_POKEMON,"rw");
+	fclose(archivoVacioMetadata);
+
+	t_config* metaPokemon = config_create(RUTA_DE_POKEMON);
+	config_set_value(metaPokemon,"DIRECTORY","N");
+	config_set_value(metaPokemon,"SIZE","0");
+	config_set_value(metaPokemon,"BLOCKS","[]");
+	config_set_value(metaPokemon,"OPEN","N");
+	config_save(metaPokemon);
+	config_destroy(metaPokemon);
+	free(path_pokemon);
+}
+
+void abrirArchivo(char* nombrePokemon){
+	// CAPAZ UN MUTEX
+	t_config* metaPokemon = obtener_metadata_de_pokemon(nombrePokemon);
+	config_set_value(metaPokemon,"OPEN","Y");
+	config_save(metaPokemon);
+	config_destroy(metaPokemon);
+}
+
+void cerrarArchivo(char* nombrePokemon){
+	// CAPAZ UN MUTEX
+	t_config* metaPokemon = obtener_metadata_de_pokemon(nombrePokemon);
+	config_set_value(metaPokemon,"OPEN","N");
+	config_save(metaPokemon);
+	config_destroy(metaPokemon);
+}
+
+bool archivoAbierto(char* nombrePokemon){
+	int valor;
+	t_config* metaPokemon = obtener_metadata_de_pokemon(nombrePokemon);
+	char *estaAbierto = config_get_string_value(metaPokemon,"OPEN");
+	if(string_equals_ignore_case(estaAbierto,"Y")) valor = 1;
+	else valor = 0;
+
+	config_destroy(metaPokemon);
+	return valor;
 }
--- GameCard/GameCard.h
+++ GameCard/GameCard.h
@@ -11,34 +11,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<commons/log.h>
+#include<stdint.h>
 #include<sys/socket.h>
 #include<unistd.h>
 #include<netdb.h>
+#include<commons/log.h>
 #include<commons/collections/list.h>
 #include<commons/config.h>
+#include<commons/string.h>
 #include<string.h>
 #include<pthread.h>
 #include "../Serializacion/Serializacion.h"
-
-typedef enum
-{
-	GET=1,
-	CATCH=2,
-	NEW=3,
-}op_code;
-
-typedef struct
-{
-	int size;
-	void* stream;
-} t_buffer;
-
-typedef struct
-{
-	op_code codigo_operacion;
-	t_buffer* buffer;
-} t_paquete;
 
 //VARIABLES
 
@@ -62,5 +45,42 @@ void atender_cliente(int *socket);
 void procesar_solicitud(int cod_op, int cliente_fd);
 void leerArchivoDeConfiguracion(void);
 void setearValoresDeGame(t_config *confg);
+
+////////////
+// Sobre el FS
+////////////
+
+uint32_t TAMANIO_DE_BLOQUE;
+uint32_t BLOQUES_DE_BITMAP;
+char *MAGIC_NUMBER;
+char *RUTA_DE_BLOQUES;
+char *RUTA_DE_FILES;
+char *RUTA_DE_POKEMON;
+char *RUTA_DE_METADATA_MONTAJE;
+t_config *config_fs;
+
+// PARA INICIAR EL PUNTO DE MONTAJE
+void iniciar_punto_de_montaje(char *puntoDeMontaje);
+void inicio_default(char* puntoDeMontaje);
+void carpetas_iniciar(char* puntoDeMontaje);
+void carga_config_fs();
+void bloques_iniciar();
+
+// Utilidades varias
+
+void mostrarRutas(void);
+char* obtener_nombre_de_archivo(char *path);
+char* obtener_direccion_metadata(char *path);
+t_config* obtener_metadata_de_ruta(char *path);
+int esDirectorio(char* ruta);
+
+// Utilidades para pokemon
+t_config* obtener_metadata_de_pokemon(char *nombrePokemon);
+bool existePokemon(char* nombrePokemon);
+void crearPokemon(char* nombrePokemon);
+bool archivoAbierto(char* nombrePokemon);
+void abrirArchivo(char* nombrePokemon);
+void cerrarArchivo(char* nombrePokemon);
+
 
 #endif /* GAMECARD_GAMECARD_H_ */
