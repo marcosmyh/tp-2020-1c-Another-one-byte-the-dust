2b1ea2e67593958cf50bd6edf31c49204c6a151a
marcosmyh <marcos_myh@hotmail.com>
2020-07-18 15:59:12 -0300

    Agrego el script para el deploy

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+cd /home/utnso/workspace
+git clone https://github.com/sisoputnfrba/tp-2020-1c-Another-one-byte-the-dust.git
+git clone https://github.com/sisoputnfrba/delibird-pruebas.git
+cd tp-2020-1c-Another-one-byte-the-dust
+cd Serializacion
+make
+cd ..
+make
+cd ..
+cd delibird-pruebas
+mv buddy_basico.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv cargar_memoria_complejo.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv cargar_memoria_simple.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv catch_charmander.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv catch_pokemon_escenario_1_s1.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv catch_pokemon_escenario_1_s2.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv compactacion_basico.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv consolidacion_basico.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv espera_circular_basico.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv new_pikachu.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv new_pokemon_antes_team.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv new_pokemon_post_team.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv new_pokemons_varios.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+mv prueba_final_team.sh /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/GameBoy
+
