676b7e19b9f697cadc1314a98431f0751fbba46d
marcosmyh <marcos_myh@hotmail.com>
2020-08-07 19:57:13 -0300

    Agrego configs y un script para el team

---
+++ Configs/ScriptPruebaFinalTeam.sh
@@ -0,0 +1,10 @@
+mv /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/Team/Team.config  /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/Configs
+mv "/home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/Configs/Team-1 Con Broker.config"  "/home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/Team"
+cd ..
+cd Team
+mv "Team-1 Con Broker.config" "Team.config"
+mv "/home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/Configs/Team-2 Con Broker.config"  "/home/utnso/Escritorio"
+cd /home/utnso/Escritorio
+mv "Team-2 Con Broker.config" "Team.config"
+cd /home/utnso/workspace/tp-2020-1c-Another-one-byte-the-dust/Team
+cp Team /home/utnso/Escritorio
